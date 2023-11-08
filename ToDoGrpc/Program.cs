86bf77f1a6a76d1b95dfd6f11790b805bef19f06
Gururajraibagi <Gururajraibagi7@gmail.com>
2023-11-08 23:54:49 +0530

    added google transcoding

--- ToDoGrpc/Program.cs
+++ ToDoGrpc/Program.cs
@@ -7,8 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Additional configuration is required to successfully run gRPC on macOS.
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 builder.Services.AddDbContext<AppDbContext>(opt=> opt.UseSqlite("Data Source=ToDoDatabase.db"));
+
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
 
